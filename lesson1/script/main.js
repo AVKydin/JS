db5ca3cc11b3b553ec25a77c1c127fedd5a7b19b
AVKydin <tlalok@ukr.net>
2023-01-30 23:27:32 +0200

    end HWL1

---
+++ lesson1/script/main.js
@@ -0,0 +1,93 @@
+{
+    let hello = 'hello';
+    let owu = 'owu';
+    let com = 'com';
+    let ua = 'ua';
+    let one = 1;
+    let ten = 10;
+    let num1 = -999;
+    let num2 = 123;
+    let num3 = 3.14;
+    let num4 = 2.7;
+    let num5 = 16;
+    let t = true;
+    let f = false;
+
+    console.log(hello);
+    console.log(owu);
+    console.log(com);
+    console.log(ua);
+    console.log(one);
+    console.log(ten);
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+    console.log(num5);
+    console.log(t);
+    console.log(f);
+}
+console.log('**************************************');
+
+{
+    let firstName = 'Anatoliy';
+    let middleName = 'Volodimirovich';
+    let lastName = 'Kudin';
+
+    let person = `${firstName} ${middleName} ${lastName}`
+
+    console.log(person);
+}
+console.log('**************************************');
+{
+    let a = 100;
+    let b = '100';
+    let c = true;
+
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+console.log('**************************************');
+{
+    let firstName = prompt(`Введіть ім'я`);
+    let middleName = prompt(`Введіть по батькові`);
+    let lastName = prompt(`Введіть прізвище`);
+
+    let person = `${firstName} ${middleName} ${lastName}`
+
+    console.log(person);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
