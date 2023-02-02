a5a20434fcb5bd602181226d9e96c8b9a6c9c41a
AVKydin <tlalok@ukr.net>
2023-02-02 09:05:07 +0200

    end HWL2

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,5 +1,6 @@
 // Масиви та об'єкти:
 // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 
 let arr1 = [10, true, -55, 'string', false, 'infinity', "стоп'ятсот", ' ', {}, []];
 
@@ -13,7 +14,9 @@ console.log(arr1[6]);
 console.log(arr1[7]);
 console.log(arr1[8]);
 console.log(arr1[9]);
+
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
 let book1 = {
     title: 'THE LORD OF THE RINGS: THE FELLOWSHIP OF THE RING',
     pageCount: 448,
@@ -29,7 +32,9 @@ let book3 = {
     pageCount: 344,
     genre: 'пригоди'
 }
-// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+
 let book4 = {
     title: 'THE LORD OF THE RINGS: THE FELLOWSHIP OF THE RING',
     pageCount: 448,
@@ -115,11 +120,11 @@ console.log(arrUser[7].password);
 console.log(arrUser[8]['password']);
 console.log(arrUser[9].password);
 
-
 // Логічні розгалуження:
 //     - Є змінна х, якій ви надаєте довільне числове значення.
-let x = +prompt('введіть довільне число');
 //     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+let x = +prompt('введіть довільне число');
 if (x !== 0) {
     console.log('Вірно')
 } else {
@@ -128,6 +133,7 @@ if (x !== 0) {
 }
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю або четверту частину години).
+
 let time = +prompt('введіть число від 0 до 59');
 if (time >= 0 && time < 15) {
     console.log('перша чверть години')
@@ -140,6 +146,7 @@ if (time >= 0 && time < 15) {
 }
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
 let day = +prompt('введіть число від 1 до 31');
 if (day >= 1 && day < 11) {
     console.log('перша декада місяця')
@@ -150,6 +157,7 @@ if (day >= 1 && day < 11) {
 }
 
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
 let numDay = parseInt(prompt('введіть порядковий номер дня тижня (від 0 до 6)'));
 switch(numDay) {
     case 0:
@@ -181,10 +189,75 @@ switch(numDay) {
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
 
+let num1 = +prompt('Введіть перше число');
+let num2 = parseInt(prompt('Введіть друге число'));
+if(num1>num2){
+    console.log(num1)
+} else if(num1<num2){
+    console.log(num2)
+}else {
+    console.log('числа рівні')
+}
 
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
 
+let X = prompt('Введіть що-небудь');
+if (X === '0' || X === null || X === undefined || X === NaN || X === '') {
+    X = "default";
+}
 
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
-//         Footer
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
+if(coursesAndDurationArray[0].monthDuration > 5){
+    console.log("Супер");
+}
+if(coursesAndDurationArray[1]['monthDuration'] > 5){
+    console.log("Супер");
+}
+if(coursesAndDurationArray[2].monthDuration > 5){
+    console.log("Супер");
+}
+if(coursesAndDurationArray[3]['monthDuration'] > 5){
+    console.log("Супер");
+}
+if(coursesAndDurationArray[4].monthDuration > 5){
+    console.log("Супер");
+}
+if(coursesAndDurationArray[5]['monthDuration'] > 5){
+    console.log("Супер");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
