70e9fa60c21c35930dbc605160f3d7180cda8122
AVKydin <tlalok@ukr.net>
2023-02-08 00:11:40 +0200

    for today, it is enough to complete the additional tasks of the third lesson

--- lesson3/additional.js
+++ lesson3/additional.js
@@ -1,12 +1,12 @@
 // --створити масив з:
 //     - з 5 числових значень
-const arrNum = [3,8,22,342,4675];
+const arrNum = [3, 8, 22, 342, 4675];
 console.log(arrNum);
 // - з 5 стічкових значень
-const arrStrings = ['як', 'не', 'крути','русні','кабздець'];
+const arrStrings = ['як', 'не', 'крути', 'русні', 'кабздець'];
 console.log(arrStrings)
 // - з 5 значень стрічкового, числового та булевого типу
-const arrMixed = [2,24, true, 'Hi', false];
+const arrMixed = [2, 24, true, 'Hi', false];
 console.log(arrMixed);
 // - та вивести його в консоль
 //
@@ -20,31 +20,184 @@ console.log(arrEmpty)
 
 // - є масив [2,17,13,6,22,31,45,66,100,-18] :
 // 1. перебрати його циклом while
-const arrNumber = [2,17,13,6,22,31,45,66,100,-18];
+const arrNumber = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 
-let j = 0;
-console.log(arrNumber[j]);
+{
+    let item = 0;
+    while (item < arrNumber.length) {
+        console.log(arrNumber[item]);
+        item++;
+    }
+}
 
-// while (i<arrNumber.length){
-//     console.log(arrNumber[i]);
-//     i++
-// }
 //     2. перебрати його циклом for
+{
+    for (let i = 0; i < arrNumber.length; i++) {
+        console.log(arrNumber[i]);
+    }
+}
+
 //     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+{
+    let item = 1
+    while (item < arrNumber.length) {
+        console.log(arrNumber[item]);
+        item += 2;
+    }
+}
+
 // 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+{
+    for (let i = 1; i < arrNumber.length; i += 2) {
+        console.log(arrNumber[i]);
+    }
+}
+
 // 5. перебрати циклом while та вивести  числа тільки парні  значення
+{
+    let item = 0;
+    while (item < arrNumber.length) {
+        console.log(arrNumber[item]);
+        item += 2;
+    }
+}
 // 6. перебрати циклом for та вивести  числа тільки парні  значення
+{
+    for (let i = 0; i < arrNumber.length; i += 2) {
+        console.log(arrNumber[i]);
+    }
+}
 // 7. замінити кожне число кратне 3 на слово "okten"
+{
+    for (let i = 0; i < arrNumber.length; i += 1) {
+        if (arrNumber[i] % 3 === 0) {
+            arrNumber[i] = "okten";
+        }
+    }
+}
 // 8. вивести масив в зворотньому порядку.
+{
+    let arrNumberReverse = [];
+    for (let i = arrNumber.length - 1; i >= 0; i -= 1) {
+        let j = (arrNumber.length - 1) - i;
+        arrNumberReverse[j] = arrNumber[i];
+    }
+    console.log(arrNumberReverse)
+}
 // 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
 
+{
+    const arrNumber = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+    // 1. перебрати його циклом while
+    {
+        let item = arrNumber.length - 1;
+        while (item >= 0) {
+            console.log(arrNumber[item]);
+            item--;
+        }
+    }
+    //     2. перебрати його циклом for
+    {
+        for (let i = arrNumber.length - 1; i >= 0; i--) {
+            console.log(arrNumber[i]);
+        }
+    }
+    //     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+    {
+        let item = arrNumber.length - 1;
+        while (item >= 0) {
+            if (item % 2 !== 0) {
+                console.log(arrNumber[item]);
+            }
+            item -= 1;
+        }
+    }
+    // 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+    {
+        for (let i = arrNumber.length-1; i >=0; i -= 1) {
+            if (i % 2 !== 0) {
+                console.log(arrNumber[i]);
+            }
+        }
+    }
+    // 5. перебрати циклом while та вивести  числа тільки парні  значення
+    {
+        let item = arrNumber.length-1;
+        while (item >=0) {
+            if(item%2===0){
+            console.log(arrNumber[item]);
+            }
+            item --;
+        }
+    }
+    // 6. перебрати циклом for та вивести  числа тільки парні  значення
+    {
+        for (let i = arrNumber.length-1; i >=0; i --) {
+            if(i%2===0){
+                console.log(arrNumber[i]);
+            }
+        }
+    }
+    // 7. замінити кожне число кратне 3 на слово "okten"
+    {
+        for (let i = arrNumber.length-1; i >=0; i --) {
+            if (arrNumber[i] % 3 === 0) {
+                arrNumber[i] = "okten";
+            }
+        }
+    }
+}
 
 // - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+{
+    const arr = [0,1,2,3,4,5,6,7,8,9,];
+    for (const number of arr) {
+        console.log(number);
+    }
+}
+
 // - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+{
+    const arr = ['asd2','cxx','qwe2','vbnv','hfg4543','ytu','nm,n','ggf','e543656','fgdg'];
+    for (const string of arr) {
+        console.log(string);
+    }
+}
 // - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+{
+    const arr = [234, true, null, '15454', Infinity, 4564, undefined, false, 'dzawq', 0];
+    for (const arrElement of arr) {
+        console.log(arrElement);
+    }
+}
 // - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+{
+    const arr = [234, true, '15454', Infinity, 4564, false, 'dzawq', 0, '', true];
+    for (const arrElement of arr) {
+        if(typeof arrElement === "boolean"){
+            console.log(arrElement);
+        }
+    }
+}
 // - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+{
+    const arr = [234, true, '15454', Infinity, 4564, false, 'dzawq', 0, '', true];
+    for (const arrElement of arr) {
+        if(typeof arrElement === "number"){
+            console.log(arrElement);
+        }
+    }
+}
 // - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+
+{
+    const arr = [234, true, '15454', Infinity, 4564, false, 'dzawq', 0, ' ', true];
+    for (const arrElement of arr) {
+        if(typeof arrElement === 'string'){
+            console.log(arrElement);
+        }
+    }
+}
 
 
 // - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
@@ -53,7 +206,6 @@ console.log(arrNumber[j]);
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
 // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-
 
 
 // стоврити масив книжок (назва, кількість сторінок, автори , жанри).
