dbd26314306f2815ac615200b2dbd8d04d6ee92d
AVKydin <tlalok@ukr.net>
2023-03-05 13:51:06 +0200

    закінчив міні-проект

--- mini-project/post-details.js
+++ mini-project/post-details.js
@@ -9,13 +9,13 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(responce => responce.json())
     .then(postInfo => {
 
-        console.log(postInfo)
         const id = postInfo.id;
         const title = postInfo.title;
         const body = postInfo.body;
         const userId = postInfo.userId;
 
         const postDiv = document.createElement('div');
+        postDiv.classList.add('postDiv')
         document.body.appendChild(postDiv);
         const postId = document.createElement('p');
         postId.innerText = `id: ${id}`;
@@ -26,28 +26,42 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         const postUserId = document.createElement('p');
         postUserId.innerText = `userId: ${userId}`;
 
+        const button = document.createElement('button');
+        button.innerHTML = 'Вибрати інший пост'
+        const a = document.createElement('a');
+
+        a.href = `user-details.html?info=` + JSON.stringify(`${userId}`);
+        a.appendChild(button)
+
+        document.body.appendChild(a)
 
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
             .then(responce => responce.json())
             .then(comments => {
-                const comentsBlock = document.createElement('div');
-                document.body.appendChild(comentsBlock);
-                 for (const comment of comments) {
-                        const commentId = document.createElement('p');
-                        commentId.innerText = `comment id: ${comment.id}`;
-                        const commentPostId = document.createElement('p');
-                        commentPostId.innerText = `comment postId: ${comment.postId}`;
-                        const commentName = document.createElement('p');
-                        commentName.innerText = `comment name: ${comment.name}`;
-                        const commentBody = document.createElement('p');
-                        commentBody.textContent = `comment body: ${comment.body}`;
-                        const commentEmail = document.createElement('p');
-                        commentEmail.textContent = `comment email: ${comment.email}`;
+                    const comentsBlock = document.createElement('div');
+                    comentsBlock.classList.add('comentsBlock')
+                    document.body.appendChild(comentsBlock);
+                for (const comment of comments) {
+                    const comentBlock = document.createElement('div');
+                    comentBlock.classList.add('comentBlock')
+                    comentsBlock.appendChild(comentBlock);
+                    const commentId = document.createElement('p');
+                    commentId.innerText = `comment id: ${comment.id}`;
+                    const commentPostId = document.createElement('p');
+                    commentPostId.innerText = `comment postId: ${comment.postId}`;
+                    const commentName = document.createElement('p');
+                    commentName.innerText = `comment name: ${comment.name}`;
+                    const commentBody = document.createElement('p');
+                    commentBody.textContent = `comment body: ${comment.body}`;
+                    const commentEmail = document.createElement('p');
+                    commentEmail.textContent = `comment email: ${comment.email}`;
 
-                        comentsBlock.append(commentId,commentPostId, commentName, commentBody, commentEmail)
+                    comentBlock.append(commentId, commentPostId, commentName, commentBody, commentEmail)
                 }
             })
 
 
-        postDiv.append(postId, postTitle, postBody, postUserId)
+        postDiv.append(postId, postUserId, postTitle, postBody)
     })
+
+//     post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
--- mini-project/user-details.js
+++ mini-project/user-details.js
@@ -45,32 +45,45 @@ fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 
         const btn = document.createElement('button');
         btn.innerText = 'post of current user'
+
         btn.onclick = function () {
+            if (!!document.querySelector('.postsBlock')){
+                btn.disabled = true;
+                btn.innerText = 'вже виведено всі пости'
+                btn.classList.add('btnDis')
+            }else {
+
             fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
                 .then(responce => responce.json())
                 .then(userPosts => {
-                    console.log(userPosts);
-                            const postsBlock = document.createElement('div');
-                            postsBlock.classList.add('postsBlock');
-                            document.body.appendChild(postsBlock);
-                        for (const post of userPosts) {
-                            const postBlock = document.createElement('div');
-                            postBlock.classList.add('postBlock');
-                            const title = post.title;
-                            const titleBlock = document.createElement('h4');
-                            titleBlock.innerText = title;
-                            postsBlock.appendChild(postBlock);
-                                const button = document.createElement('button');
-                                button.innerHTML = 'More info post'
-                                const a = document.createElement('a');
-                                a.href = `post-details.html?info=` + JSON.stringify(post.id);
-                                a.style.display = 'block'
-                                a.appendChild(button);
-                            postBlock.append(titleBlock,a);
-                                // titleBlock.appendChild(a)
-                        }
+
+
+                    const postsBlock = document.createElement('div');
+                    postsBlock.classList.add('postsBlock');
+                    document.body.appendChild(postsBlock);
+                    for (const post of userPosts) {
+                        const postBlock = document.createElement('div');
+                        postBlock.classList.add('postBlock');
+                        const title = post.title;
+                        const titleBlock = document.createElement('h4');
+                        titleBlock.innerText = title;
+                        postsBlock.appendChild(postBlock);
+                        const button = document.createElement('button');
+                        button.innerHTML = 'More info post'
+                        const a = document.createElement('a');
+                        a.href = `post-details.html?info=` + JSON.stringify(post.id);
+                        a.style.display = 'block'
+                        a.appendChild(button);
+                        postBlock.append(titleBlock, a);
+
+
+                    }
                 })
+            }
         }
 
         userDiv.append(userId, userNS, userAddress, userPhone, userEmail, userWebsite, userCompany, btn)
     })
+
+//     user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
+//     блоки з короткою іфною про post - в ряд по 5 .
